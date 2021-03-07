d54e88ba4e2c40dd5e14212733527641cd46da01
tinku singh <singhtinku0803@gmil.com>
2021-03-07 14:16:33 +0530

    quiz website done

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html> 
+<html dir="ltr" lang="en-GB">
+    <head>
+ <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
+    <link rel="stylesheet" href="bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="style.css" type="text/css">    
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <title>Online Quiz on General Knowledge | Playquiz2win</title>
+  </head>
+
+<body class="stretched no-transition device-xl gototop-active" aria-hidden="false">
+
+<div id="wrapper" class="clearfix">
+<div class="clear"></div>
+
+<!-- Content
+============================================= -->
+<section id="content" style="margin-bottom: 0px;">
+	<div class="content-wrap">
+		<div class="section header-stick bottommargin-lg clearfix" style="padding: 20px 0;">
+            <div>
+                <div class="container clearfix">
+                    
+                            
+						
+<!-- Post Content
+============================================= -->
+<div class="postcontent nobottommargin clearfix">
+	 
+<hr style="text-align:right;margin-right:0">
+<h3 align="center">Take 10 Questions Online Quiz on General Knowledge below:</h3>
+<hr style="text-align:right;margin-right:0">
+		
+<form method="post" id="quizForm" action="" style="width:100%; text-align:justify;" novalidate="novalidate">
+
+
+ 
+<div class="quiz-section"> 
+<h4> <span class="quiz-no">1</span> Proclamation of emergency under Article 352, when Lok Sabha stands dissolved, has to be approved by :</h4> 
+<label><input type="radio" class="form-radio" name="answer[792]" value="A" required=""> <span>A) Rajya Sabha and then will continue till the reconstitution of new Lok Sabha which must approve it wi</span></label> 
+<label><input type="radio" class="form-radio" name="answer[792]" value="B"> <span>B) New Lok Sabha within 6 months of its Constitution</span></label> 
+<label><input type="radio" class="form-radio" name="answer[792]" value="C"> <span>C) Lok Sabha in next session after six months</span></label> 
+<label><input type="radio" class="form-radio" name="answer[792]" value="D"> <span>D) Rajya Sabha only</span></label> 
+<label for="answer[792]" class="error">Answer is required</label> 
+</div> 
+ 
+<div class="quiz-section"> 
+<h4> <span class="quiz-no">2</span> Water is neither acidic nor alkaline because:</h4> 
+<label><input type="radio" class="form-radio" name="answer[1528]" value="A" required=""> <span>A) it cannot accept or donate protons</span></label> 
+<label><input type="radio" class="form-radio" name="answer[1528]" value="B"> <span>B) it boils at high temperature</span></label> 
+<label><input type="radio" class="form-radio" name="answer[1528]" value="C"> <span>C) it can dissociate into equal number of hydrogen ions</span></label> 
+<label><input type="radio" class="form-radio" name="answer[1528]" value="D"> <span>D) it cannot donate or accept electrons</span></label> 
+<label for="answer[1528]" class="error">Answer is required</label> 
+</div> 
+ 
+<div class="quiz-section"> 
+<h4> <span class="quiz-no">3</span> The economy of the Indus Valley people was based on?</h4> 
+<label><input type="radio" class="form-radio" name="answer[1023]" value="A" required=""> <span>A) Agriculture</span></label> 
+<label><input type="radio" class="form-radio" name="answer[1023]" value="B"> <span>B) Trade and Commerce</span></label> 
+<label><input type="radio" class="form-radio" name="answer[1023]" value="C"> <span>C) Crafts</span></label> 
+<label><input type="radio" class="form-radio" name="answer[1023]" value="D"> <span>D) All of these</span></label> 
+<label for="answer[1023]" class="error">Answer is required</label> 
+</div> 
+ 
+<div class="quiz-section"> 
+<h4> <span class="quiz-no">4</span> Where do Bhagirathi and Alakananda join Ganga?</h4> 
+<label><input type="radio" class="form-radio" name="answer[1624]" value="A" required=""> <span>A) Gangotri</span></label> 
+<label><input type="radio" class="form-radio" name="answer[1624]" value="B"> <span>B) Karanprayag</span></label> 
+<label><input type="radio" class="form-radio" name="answer[1624]" value="C"> <span>C) Devprayag</span></label> 
+<label><input type="radio" class="form-radio" name="answer[1624]" value="D"> <span>D) Rudraprayag</span></label> 
+<label for="answer[1624]" class="error">Answer is required</label> 
+</div> 
+ 
+<div class="quiz-section"> 
+<h4> <span class="quiz-no">5</span> For regulation of the Insurance Trade in the country the Government has formed:</h4> 
+<label><input type="radio" class="form-radio" name="answer[355]" value="A" required=""> <span>A) SEBI</span></label> 
+<label><input type="radio" class="form-radio" name="answer[355]" value="B"> <span>B) Reserve Bank of India</span></label> 
+<label><input type="radio" class="form-radio" name="answer[355]" value="C"> <span>C) Insurance Regulatory and Development Authority</span></label> 
+<label><input type="radio" class="form-radio" name="answer[355]" value="D"> <span>D) General Insurance Corporation</span></label> 
+<label for="answer[355]" class="error">Answer is required</label> 
+</div> 
+ 
+<div class="quiz-section"> 
+<h4> <span class="quiz-no">6</span> From which period large scale land grants were given to officers and other important person?</h4> 
+<label><input type="radio" class="form-radio" name="answer[374]" value="A" required=""> <span>A) Gupta period</span></label> 
+<label><input type="radio" class="form-radio" name="answer[374]" value="B"> <span>B) Kushan period</span></label> 
+<label><input type="radio" class="form-radio" name="answer[374]" value="C"> <span>C) Mauryan period</span></label> 
+<label><input type="radio" class="form-radio" name="answer[374]" value="D"> <span>D) None of these</span></label> 
+<label for="answer[374]" class="error">Answer is required</label> 
+</div> 
+ 
+<div class="quiz-section"> 
+<h4> <span class="quiz-no">7</span> National Library, the largest library in India is in which state?</h4> 
+<label><input type="radio" class="form-radio" name="answer[701]" value="A" required=""> <span>A) Maharashtra</span></label> 
+<label><input type="radio" class="form-radio" name="answer[701]" value="B"> <span>B) West Bengal</span></label> 
+<label><input type="radio" class="form-radio" name="answer[701]" value="C"> <span>C) Karnataka</span></label> 
+<label><input type="radio" class="form-radio" name="answer[701]" value="D"> <span>D) Tamil Nadu</span></label> 
+<label for="answer[701]" class="error">Answer is required</label> 
+</div> 
+ 
+<div class="quiz-section"> 
+<h4> <span class="quiz-no">8</span> Who among the following is known for his work on medicine during the Gupta period?</h4> 
+<label><input type="radio" class="form-radio" name="answer[2186]" value="A" required=""> <span>A) Saumilla</span></label> 
+<label><input type="radio" class="form-radio" name="answer[2186]" value="B"> <span>B) Sudraka</span></label> 
+<label><input type="radio" class="form-radio" name="answer[2186]" value="C"> <span>C) Shaunaka</span></label> 
+<label><input type="radio" class="form-radio" name="answer[2186]" value="D"> <span>D) Susrutha</span></label> 
+<label for="answer[2186]" class="error">Answer is required</label> 
+</div> 
+ 
+<div class="quiz-section"> 
+<h4> <span class="quiz-no">9</span> Fundamental Rights can be classified into?</h4> 
+<label><input type="radio" class="form-radio" name="answer[377]" value="A" required=""> <span>A) 5 groups</span></label> 
+<label><input type="radio" class="form-radio" name="answer[377]" value="B"> <span>B) 6 groups</span></label> 
+<label><input type="radio" class="form-radio" name="answer[377]" value="C"> <span>C) 7 groups</span></label> 
+<label><input type="radio" class="form-radio" name="answer[377]" value="D"> <span>D) 8 groups</span></label> 
+<label for="answer[377]" class="error">Answer is required</label> 
+</div> 
+ 
+<div class="quiz-section"> 
+<h4> <span class="quiz-no">10</span> Mahatma Gandhi was born at which place?</h4> 
+<label><input type="radio" class="form-radio" name="answer[652]" value="A" required=""> <span>A) Sabarmati</span></label> 
+<label><input type="radio" class="form-radio" name="answer[652]" value="B"> <span>B) Ahmedabad</span></label> 
+<label><input type="radio" class="form-radio" name="answer[652]" value="C"> <span>C) Porbander</span></label> 
+<label><input type="radio" class="form-radio" name="answer[652]" value="D"> <span>D) Wardha</span></label> 
+<label for="answer[652]" class="error">Answer is required</label> 
+</div> 
+
+
+<button class="btn btn-primary"><span>Check Result</span></button>
+</form>  
+  
+
+<br>
+                        
+				
+</div><!-- .postcontent end -->
+		 
+</div><!-- Div container clearfix ends here -->
+</div>
+</div><!-- Div class="section header-stick bottommargin-lg clearfix" ends here -->
+</div><!-- Div contents wrap ends here -->
+	 
+</div><!-- #wrapper end -->
+     
